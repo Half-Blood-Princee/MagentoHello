7fccca5be28cdb0ef4102857c5ab30cfaf5430ef
Daniil <dse6023@gmail.com>
2021-08-02 23:00:19 +0300

    Layout show text

--- app/code/Learning/HelloModule/Controller/Index/Index.php
+++ app/code/Learning/HelloModule/Controller/Index/Index.php
@@ -4,11 +4,12 @@ namespace Learning\HelloModule\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action implements HttpGetActionInterface
 {
     public function execute()
     {
-        echo "Hello Daniil";
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
